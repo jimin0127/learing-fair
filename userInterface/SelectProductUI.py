aa8dbf10104ce8e87fac28e08b4072099956e18f
jimin0127 <ckdgh4662@naver.com>
2025-05-24 23:32:40 +0900

    SelectProductUI 리팩터링

--- userInterface/SelectProductUI.py
+++ userInterface/SelectProductUI.py
@@ -11,25 +11,29 @@ from util.get_resource_path import get_resource_path
 class ProductItem(QWidget):
     def __init__(self, product, open_product_detail):
         super().__init__()
+
         self.product = product
         self.open_product_detail = open_product_detail
 
+        self.unit_ui()
+
+    def unit_ui(self):
         card_frame = QFrame()
         card_frame.setFixedHeight(280)
 
         card_layout = QVBoxLayout()
         card_layout.setAlignment(Qt.AlignCenter)
 
-        pixmap = QPixmap(get_resource_path(product["image_path"])).scaled(120, 120, Qt.KeepAspectRatio)
+        pixmap = QPixmap(get_resource_path(self.product["image_path"])).scaled(120, 120, Qt.KeepAspectRatio)
         image_label = QLabel()
         image_label.setPixmap(pixmap)
         image_label.setAlignment(Qt.AlignCenter)
 
-        name_label = QLabel(product["name"])
+        name_label = QLabel(self.product["name"])
         name_label.setObjectName("headerLabel")
         name_label.setWordWrap(True)
-        price_label = QLabel(format_price(product['price']))
-        brand_label = QLabel(product["brand"])
+        price_label = QLabel(format_price(self.product['price']))
+        brand_label = QLabel(self.product["brand"])
         name_label.setAlignment(Qt.AlignCenter)
         price_label.setAlignment(Qt.AlignCenter)
         brand_label.setAlignment(Qt.AlignCenter)
@@ -37,7 +41,7 @@ class ProductItem(QWidget):
         action_layout = QHBoxLayout()
         buy_btn = QPushButton("구매하기")
         buy_btn.setFixedSize(150, 30)
-        buy_btn.clicked.connect(lambda: open_product_detail(self.product))
+        buy_btn.clicked.connect(lambda: self.open_product_detail(self.product))
         action_layout.addWidget(buy_btn)
 
         card_layout.addWidget(image_label)
@@ -54,10 +58,16 @@ class ProductItem(QWidget):
 class SelectProductUI(QWidget):
     def __init__(self, open_product_detail, selected_category, on_back):
         super().__init__()
+
         self.product_service = ProductService()
         self.selected_category = selected_category
+        self.open_product_detail = open_product_detail
         self.on_back = on_back
-        products = self.product_service.find_all_by_category(selected_category)
+
+        self.unit_ui()
+
+    def unit_ui(self):
+        products = self.product_service.find_all_by_category(self.selected_category)
 
         top_bar_layout = QGridLayout()
 
@@ -66,9 +76,9 @@ class SelectProductUI(QWidget):
         top_bar_layout.setColumnStretch(2, 1)
 
         back_btn = QPushButton("← 뒤로가기")
-        back_btn.clicked.connect(on_back)
+        back_btn.clicked.connect(self.on_back)
 
-        category_label = QLabel(selected_category)
+        category_label = QLabel(self.selected_category)
         category_label.setAlignment(Qt.AlignCenter)
         category_label.setStyleSheet("font-size: 16px; font-weight: bold;")
 
@@ -88,7 +98,7 @@ class SelectProductUI(QWidget):
             row = index // columns_per_row
             col = index % columns_per_row
 
-            item = ProductItem(product, open_product_detail)
+            item = ProductItem(product, self.open_product_detail)
             grid_layout.addWidget(item, row, col)
 
         scroll_widget.setLayout(grid_layout)
@@ -97,4 +107,5 @@ class SelectProductUI(QWidget):
         main_layout = QVBoxLayout()
         main_layout.addLayout(top_bar_layout)
         main_layout.addWidget(scroll_area)
+
         self.setLayout(main_layout)
